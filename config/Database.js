6aa05597ce013fe6dccc9ffbd2842758f5338d64
alungggggg <aldinoalungputraanugraha@gmail.com>
2024-09-18 14:17:23 +0700

    db

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("split", "root", "", {
+const db = new Sequelize("dongeng", "root", "", {
     host: "localhost",
     dialect: "mysql"
 })
