d23a07a53a89499ba42698adb42cd543605cdc39
Dani Ardiansyach <ardiansyachd5@gmail.com>
2024-09-24 16:26:03 +0700

    tod

--- config/Database.js
+++ config/Database.js
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 
 
-const db = new Sequelize({
-    database : process.env.DATABASENAME,
-    username : process.env.USERNAME,
-    password : process.env.PASSWORD,
-    host: process.env.HOSTNAME,
+const db = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USER, process.env.MYSQL_ADDON_PASSWORD,{
+    // database : process.env.DATABASENAME,
+    // username : process.env.USERNAME,
+    // password : process.env.PASSWORD,
+    host: process.env.MYSQL_ADDON_HOST,
     dialect: "mysql"
 })
 
