4951cae9e589dc25220300ee5fa5e9c08220c746
Dani Ardiansyach <ardiansyachd5@gmail.com>
2024-09-24 16:50:05 +0700

    ip

--- config/Database.js
+++ config/Database.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-
+import mysql2 from "mysql2"
 
 const db = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USER, process.env.MYSQL_ADDON_PASSWORD,{
     // database : process.env.DATABASENAME,
@@ -7,7 +7,7 @@ const db = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USE
     // password : process.env.PASSWORD,
     host: process.env.MYSQL_ADDON_HOST,
     dialect: "mysql",
-    dialectModule : require("mysql2")
+    dialectModule : mysql2
 })
 
 export default db;
