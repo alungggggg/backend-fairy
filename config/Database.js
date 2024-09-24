1a649003cfdb1b7890d80f99b2b04c962a4c75fc
Dani Ardiansyach <ardiansyachd5@gmail.com>
2024-09-24 16:44:09 +0700

    po

--- config/Database.js
+++ config/Database.js
@@ -6,7 +6,8 @@ const db = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USE
     // username : process.env.USERNAME,
     // password : process.env.PASSWORD,
     host: process.env.MYSQL_ADDON_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule : require("mysql2")
 })
 
 export default db;
